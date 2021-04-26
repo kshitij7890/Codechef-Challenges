a9402def21f46aa891e72779fca633bb49b47c02
kshitij7890 <kshitijsingla7890@gmail.com>
2021-04-26 23:41:39 +0530

    CookOff commit

--- Cookoff/Nov 2020/IPCCERT.cpp
+++ Cookoff/Nov 2020/IPCCERT.cpp
@@ -12,9 +12,7 @@ int main()
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < k + 1; j++)
-        {
-            cin >> arr[i][j];
-        }
+        cin >> arr[i][j];
     }
 
     for (int i = 0; i < n; i++)
